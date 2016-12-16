b12e91cd7dbdc6998301d58d8b6f4367a667c86f
Christoph DeMaskey <c.demaskey@elsevier.com>
2016-12-16 15:11:11 -0500

    Complete Unit 2 Lesson 3 Steps 1, 2, and 3

---
+++ src/Unit-2/DoThis/Actors/ChartingMessages.cs
@@ -0,0 +1,62 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartApp.Actors
+{
+    #region Reporting
+    public class GatherMetrics { }
+
+    public class Metric
+    {
+        public Metric(string series, float counterValue)
+        {
+            CounterValue = counterValue;
+            Series = series;
+        }
+
+        public float CounterValue { get; private set; }
+        public string Series { get; private set; }
+    }
+    #endregion
+
+    #region Performance Counter Management
+    public enum CounterType
+    {
+        Cpu,
+        Memory,
+        Disk
+    }
+
+    public class SubscribeCounter
+    {
+        public SubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+            Counter = counter;
+        }
+
+        public CounterType Counter { get; private set; }
+        public IActorRef Subscriber { get; private set; }
+    }
+
+    public class UnsubscribeCounter
+    {
+        public UnsubscribeCounter(CounterType counter, IActorRef subscriber)
+        {
+            Subscriber = subscriber;
+            Counter = counter;
+        }
+
+        public CounterType Counter { get; private set; }
+        public IActorRef Subscriber { get; private set; }
+    }
+    #endregion
+
+    class ChartingMessages
+    {
+    }
+}
