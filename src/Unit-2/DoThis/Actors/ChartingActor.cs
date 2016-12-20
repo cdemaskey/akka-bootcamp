0bfb7841e2edd583929bef34174920095d197117
Christoph DeMaskey <c.demaskey@elsevier.com>
2016-12-20 14:19:34 -0500

    Complete Unit 2 Lesson 3 Step 7

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,4 +1,5 @@
 ﻿using Akka.Actor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
@@ -7,7 +8,21 @@ namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
     {
+        public const int MaxPoints = 250;
+
+        private int xPosCounter = 0;
+
         #region Messages
+
+        public class RemoveSeries
+        {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
+        }
 
         public class InitializeChart
         {
@@ -43,6 +58,9 @@ namespace ChartApp.Actors
             _chart = chart;
             _seriesIndex = seriesIndex;
 
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
+
             Receive<InitializeChart>(ic => HandleInitialize(ic));
             Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
         }
@@ -60,6 +78,12 @@ namespace ChartApp.Actors
             //delete any existing series
             _chart.Series.Clear();
 
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundries();
+
             //attempt to render the initial chart
             if (_seriesIndex.Any())
             {
@@ -70,6 +94,8 @@ namespace ChartApp.Actors
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundries();
         }
 
         private void HandleAddSeries(AddSeries series)
@@ -78,9 +104,54 @@ namespace ChartApp.Actors
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundries();
+            }
+        }
+
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) && _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints)
+                {
+                    series.Points.RemoveAt(0);
+                }
+                SetChartBoundries();
             }
         }
 
         #endregion Individual Message Type Handlers
+
+        private void SetChartBoundries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
+            }
+        }
     }
 }
